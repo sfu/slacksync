53983092f5d4a6cc8972c961650c2a8764b7da02
Graham Ballantyne <grahamb@sfu.ca>
2016-05-02 16:34:41 -0700

    Add getMaillist method

--- src/lib/__test__/maillist.js
+++ src/lib/__test__/maillist.js
@@ -1,7 +1,17 @@
 import test from 'ava'
-import {getMaillistMembers} from '../maillist'
+import {getMaillist, getMaillistMembers} from '../maillist'
 
 const {ML_TOKEN} = process.env
+
+test('getting info about a valid list should succeed', async t => {
+  const response = await getMaillist('65704', ML_TOKEN)
+  t.is(response.id, 65704)
+})
+
+test('getting info for an invalid list should throw', async t => {
+  const response = getMaillist('0000000', ML_TOKEN)
+  t.throws(response)
+})
 
 test('getting members of valid list should succeed', async t => {
   const response = await getMaillistMembers('65704', ML_TOKEN)
