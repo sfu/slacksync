8b51945aaecf9373b207edb94aa13bc16fc97139
Graham Ballantyne <grahamb@sfu.ca>
2016-05-02 16:36:29 -0700

    Refactor cli.js

--- src/cli.js
+++ src/cli.js
@@ -13,18 +13,41 @@ try {
   console.error('slacksync: error: %s', e.message)
   process.exit(1)
 }
-const requiredArgMissing = !opts.maillist || !opts.maillist_token || !opts.art_token || !opts.slack_token
 
-if (opts.help || requiredArgMissing) {
-  const help = parser.help({includeEnv: true}).trimRight()
-  console.log('usage: slacksync [OPTIONS]\n'
-            + 'options:\n'
-            + help)
-  process.exit(0)
+function printUsageAndExit(exitCode=0) {
+  /* global dedent */
+  const help = parser.help({includeEnv: true, helpWrap: false})
+  const helpstr = dedent`usage: slacksync [OPTIONS]
+                         options:
+                         ${help}
+  `
+  console.log(helpstr)
+  process.exit(exitCode)
+}
+
+if (opts.all) {
+  opts.create_users = true
+  opts.delete_users = true
+  opts.reactivate_users = true
+}
+
+const requiredArgMissing =  !opts.maillist ||
+                            !opts.maillist_token ||
+                            !opts.art_token ||
+                            !opts.slack_token
+
+if (opts.help) {
+  printUsageAndExit(0)
+}
+
+if (requiredArgMissing) {
+  console.log('Required option missing')
+  printUsageAndExit(1)
 }
 
 if (opts.version) {
   console.log(`v${require('../package.json').version}`)
+  process.exit(0)
 }
 
 slacksync(opts)
